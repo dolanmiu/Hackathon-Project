e37d22c4f09c174da5462bac2749406af2676a1e
Raphael Saunier <raphael.saunier@gmail.com>
2013-04-06 18:01:32 +0100

    Database seed

--- application/tasks/seed.php
+++ application/tasks/seed.php
@@ -6,34 +6,48 @@ class Seed_Task {
     {
 
       // Create 5 charities
-      DB::raw("DELETE FROM charities;");
+      DB::query("DELETE FROM charities;");
 
-      for($i = 0; $i<5; $i++)
+      $charities = array();
+
+      for($i = 0; $i < 10; $i++)
       {
         $charity = new Charity;
         $charity->name = "Charity #" . $i;
         $charity->save();
+        $charities[] = $charity->id;
+        
+        $j = 0;
+        while($j < 200)
+        {
+          $effect = new Effect;
+          $effect->charity_id = $charity->id;
+          $effect->min_amount = $j;
+          $j += rand(1,30);
+          $effect->max_amount = $j;
+          $effect->description = "With £" . $effect->min_amount . "-" . $effect->max_amount . " you are helping...";
+          $effect->save();
+        }
 
-        for($j = 0; $j < rand(5,20); $j++)
+      }
+
+     for($j = 0; $j < 100; $j++)
         {
           $user = new User;
           $user->first_name = "First name " . $j;
+          $user->last_name = "Last name " . $j;
           $user->save();
 
           for($k = 0; $k < rand(1,3); $k++)
           {
             $donation = new Donation;
-            $donation->amount = $k * rand(1,10)/10;
+            $donation->amount = ($k+1) * 10 * rand(1,10)/10;
             $donation->user_id = $user->id;
-            $donation->charity_id = $charity->id;
+            $donation->charity_id = $charities[array_rand($charities)];
             $donation->frequency = rand(1,3);
             $donation->save();
           }
-
         }
-
-      }
-      // Create 100 users
 
 
     }
