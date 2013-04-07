cfaaade044eded791a789e3bc178bb080f837ada
Michael Hauser-Raspe (home) <michael.hauser-raspe@cantab.net>
2013-04-07 01:32:02 +0100

    charity update

--- application/controllers/charity.php
+++ application/controllers/charity.php
@@ -25,19 +25,57 @@ class Charity_Controller extends Base_Controller
 
 
 
-  public function action_donate($id)
+  public function action_donate($id, $isRecurring=false)
   {
     $charity = Charity::find($id);
+    if (!$isRecurring) {
+      define('PAYMILL_API_HOST', 'https://api.paymill.com/v2/');
+    define('PAYMILL_API_KEY', 'e213b1e800cda98c29fb6bd7d3bde9df');
 
-    // INSERT CODE TO PROCESS PAYMENT HERE! :)
+    Autoloader::directories(array(path('app').'libraries/Paymill-PHP-master/lib'));
+
+    $token = $_POST['paymillToken'];
+    $currency = $_POST['currency'];
+    $amount = $_POST['amount'];
+
+    if ($token) {
+      // require "Services/Paymill/Transactions.php";
+      $transactionsObject = new Services_Paymill_Transactions(PAYMILL_API_KEY, PAYMILL_API_HOST);
+      $params = array(
+        'amount'      => $amount*100,  // Cent!
+        'currency'    => $currency,   // ISO 4217
+        'token'       => $token,
+        'description' => 'Test Transaction',
+      );
+      $transaction = $transactionsObject->create($params);
+
+      // echo "Transaction: ";
+      // print_r($transaction);
+
+    }
+    } else {
+      Autoloader::directories(array(path('app').'libraries/Paymill-PHP-master/lib'));
+
+    $token = $_POST['paymillToken'];
+    $currency = Input::get('currency');
+    $amount = Input::get('amount');
+    $monthly = Input::get('monthly');
+    $email = Input::get('email');
+    $card_holdername = Input::get('card-holdername');
+    
+    if ($token) {
+      $client = Paymill::clientFactoryMethod($email,$card_holdername);
+      $offer = Paymill::offerFactoryMethod($amount*100,$currency,$monthly." MONTH", "offer1");
+      $payment = Paymill::paymentFactoryMethod($token, $client);
+      $subscription = Paymill::subscriptionFactoryMethod($client, $offer, $payment);
+    }
+    
 
 
-    echo $id;
+    // echo $id;
     //return Redirect::back();
   }
-
-
-
+}
 
   public function action_edit($id)
   {
--- application/views/charity/view.blade.php
+++ application/views/charity/view.blade.php
@@ -18,7 +18,7 @@
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           <h3>Please Donate</h3>
           <div class="span12">
-           <form method='post' action='{{ URL::to_action('charity@donate', array($charity->id)) }}'>
+           <form id='payment-form' method='post' action='{{ URL::to_action('charity@donate', array($charity->id)) }}'>
            <!-- <form id="payment-form" action="<?php echo URL::to_action("payment@submitsubscription");?>" method="POST"> -->
 
 
