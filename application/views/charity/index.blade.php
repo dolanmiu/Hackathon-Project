7850b9c88d067b482af1269e8e075410512a6d12
Dolan <dolan_miu@hotmail.com>
2013-04-07 09:13:25 +0100

    Presentation
    charity/index.blade.php

--- application/views/charity/index.blade.php
+++ application/views/charity/index.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 @layout('main')
 
 @section('content')
@@ -20,4 +21,28 @@
       @endforeach 
       </div>
 </div>
+=======
+@layout('main')
+
+@section('content')
+<div class="hero-unit">
+  <h1>All charities</h1>
+  <p>Your help is greatly appreciated</p>
+</div>
+<hr>
+<div class="row">
+  <div class="span11">
+    @foreach($charities as $c)
+      <div class="span10 charity-list">
+        <div class="span2"> <img src="{{$c->image}}" height="100" class="img-polaroid" width="150"> </div>
+        <div class="span6">
+          <h2><a href="{{ URL::to_action('charity@view', array($c->id)) }}">{{ $c->name }}</a></h2>
+          <h4><a href="{{ URL::to_action('charity@map', array($c->id)) }}">Map</a></h4>
+          <h4>{{$c->description}}</h4>
+          <strong>Total donators: {{ $c->people_total }}, Donated amount: {{ $c->donation_total }}</strong> </div>
+      </div>
+      @endforeach 
+      </div>
+</div>
+>>>>>>> Presentation
 @endsection
