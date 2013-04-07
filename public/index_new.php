6cbbe87529465c3d0cc76ea61f42b06372157dc1
Dolan <dolan_miu@hotmail.com>
2013-04-07 02:09:06 +0100

    updated homepage

--- public/index_new.php
+++ public/index_new.php
@@ -1,131 +1,119 @@
-
 <!DOCTYPE html>
-<html lang="en"><head>
-    <meta charset="utf-8">
-    <title>Social Pledge - Sharing is Caring</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Social Pledge - Sharing is Caring</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
 
-    <!-- Le styles -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/css/override.css" rel="stylesheet">
-    <!-- Loading Flat UI -->
-    <link href="assets/css/flat-ui.css" rel="stylesheet">
-    <link rel="shortcut icon" href="images/favicon.ico">
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+<!-- Le styles -->
+<link href="assets/css/bootstrap.css" rel="stylesheet">
 
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
+<!-- Loading Flat UI -->
+<link href="assets/css/flat-ui.css" rel="stylesheet">
+<link rel="shortcut icon" href="images/favicon.ico">
+<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+<link href="assets/css/override.css" rel="stylesheet">
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
       <script src="../assets/js/html5shiv.js"></script>
     <![endif]-->
 
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
-  </head>
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="../assets/ico/favicon.png">
+</head>
 
-  <body>
-
-    <div class="container">
-
-      <div class="masthead">
-        <ul class="nav nav-pills pull-right">
-           <li class="active"><a href="index.php">Home</a></li>
-           <li><a href="allcharities.php">All Charites</a></li>
-           <li><a href="signin.php">Sign In</a></li>
-           <li><a href="about.php">About</a></li>
-        </ul>
-        <h3 class="muted"><img src="images/logo.png"></h3>
-      </div>
-
-      <hr>
-
-      <div class="jumbotron">
-        <img src="assets/img/homepage-jumbotron.jpg">
-        <h1>Social Giving</h1>
-        <p class="lead">Do the world a favour and donate for a range of our charities.</p>
-        <a class="btn btn-large btn-success" href="#">I am a Charity</a>
-        <a class="btn btn-large btn-success" href="#">I want to Donate</a>
-      </div>
-
-      <hr>
-
-      <div class="row-fluid marketing">
-        <div class="row">
-        <div class="span4">
-          <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Who are we?</h2>
-          <p>SocialPledge is a novel way for charities to accept one-off and regular pledge donations. It allows users to share their charitable donations on their Facebook timeline, and thus increases public awareness of the projects these donations are being used for. Additionally it provides charities with valuable demographic information about their donor base thanks to the integration with Facebook.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div><!-- /.span4 -->
-        <div class="span4">
-          <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Why?</h2>
-          <p>Because we believe in giving. We believe helping others is the greatest form of positive karma. Spread the word, donate to potentially save a life and shape the world for the greater good! Motivate your circle of friends to give, help others. Happiness is contagious!</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div><!-- /.span4 -->
-        <div class="span4">
-          <img class="img-circle" data-src="holder.js/140x140">
-          <h2>I am still not sure...</h2>
-          <p>What are you waiting for! Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div><!-- /.span4 -->
-      </div><!-- /.row -->
+<body>
+<div class="container">
+<div class="masthead">
+  <ul class="nav nav-pills pull-right">
+    <li class="active"><a href="index.php">Home</a></li>
+    <li><a href="allcharities.php">All Charites</a></li>
+    <li><a href="signin.php">Sign In</a></li>
+    <li><a href="about.php">About</a></li>
+  </ul>
+  <h3 class="muted"><img src="images/logo.png"></h3>
+</div>
 <hr class="featurette-divider">
+<div class="featurette"> <img class="featurette-image pull-right" src="assets/img/homepage-big1.jpg">
+  <h2 class="featurette-heading">Raise money. <span class="muted">To support those in need.</span></h2>
+  <a class="btn btn-large btn-success" href="#">I am a Charity</a>
+  <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
+<hr class="featurette-divider">
+<div class="featurette">
+  <h2 class="featurette-heading">Donate money. <span class="muted">See for yourself.</span></h2>
+  <a class="btn btn-large btn-success" href="#">I want to Donate</a>
+  <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
+<hr>
+<div class="indexbanner">
+<!--<div class="row-fluid indexbanner"> -->
+  <img class="screenbanner" src="assets/img/homepage-jumbotron.png">
+  <div class="row">
+    <div class="span3"> <img class="img-circle" data-src="holder.js/140x140">
+      <h2>Who are we?</h2>
+      <p>SocialPledge is a novel way for charities to accept one-off and regular pledge donations. It allows users to share their charitable donations on their Facebook timeline, and thus increases public awareness of the projects these donations are being used for. Additionally it provides charities with valuable demographic information about their donor base thanks to the integration with Facebook.</p>
+      <p><a class="btn" href="#">View details &raquo;</a></p>
+    </div>
+    <!-- /.span4 -->
+    <div class="span3"> <img class="img-circle" data-src="holder.js/140x140">
+      <h2>Why?</h2>
+      <p>Because we believe in giving. We believe helping others is the greatest form of positive karma. Spread the word, donate to potentially save a life and shape the world for the greater good! Motivate your circle of friends to give, help others. Happiness is contagious!</p>
+      <p><a class="btn" href="#">View details &raquo;</a></p>
+    </div>
+    <!-- /.span4 -->
+    <div class="span4"> <img class="img-circle" data-src="holder.js/140x140">
+      <h2>I am still not sure...</h2>
+      <p>What are you waiting for! Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+      <p><a class="btn" href="#">View details &raquo;</a></p>
+    </div>
+    <!-- /.span4 --> 
+  </div>
+  <!-- /.row -->
+  </div>
+  <hr>
+  <?php include 'footer.html'; ?>
+</div>
+<!-- /container --> 
 
-      <div class="featurette">
-        <img class="featurette-image pull-right" src="assets/img/homepage-big1.jpg">
-        <h2 class="featurette-heading">First featurette headling. <span class="muted">It'll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
+<!-- Le javascript
+    ================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+<script src="assets/js/jquery.js"></script> 
+<script src="assets/js/bootstrap-transition.js"></script> 
+<script src="assets/js/bootstrap-alert.js"></script> 
+<script src="assets/js/bootstrap-modal.js"></script> 
+<script src="assets/js/bootstrap-dropdown.js"></script> 
+<script src="assets/js/bootstrap-scrollspy.js"></script> 
+<script src="assets/js/bootstrap-tab.js"></script> 
+<script src="assets/js/bootstrap-tooltip.js"></script> 
+<script src="assets/js/bootstrap-popover.js"></script> 
+<script src="assets/js/bootstrap-button.js"></script> 
+<script src="assets/js/bootstrap-collapse.js"></script> 
+<script src="assets/js/bootstrap-carousel.js"></script> 
+<script src="assets/js/bootstrap-typeahead.js"></script> 
 
-      <hr class="featurette-divider">
-
-      <div class="featurette">
-        <h2 class="featurette-heading">Oh yeah, it's that good. <span class="muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <hr>
-    <?php include 'footer.html'; ?>
-      
-
-    </div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap-transition.js"></script>
-    <script src="assets/js/bootstrap-alert.js"></script>
-    <script src="assets/js/bootstrap-modal.js"></script>
-    <script src="assets/js/bootstrap-dropdown.js"></script>
-    <script src="assets/js/bootstrap-scrollspy.js"></script>
-    <script src="assets/js/bootstrap-tab.js"></script>
-    <script src="assets/js/bootstrap-tooltip.js"></script>
-    <script src="assets/js/bootstrap-popover.js"></script>
-    <script src="assets/js/bootstrap-button.js"></script>
-    <script src="assets/js/bootstrap-collapse.js"></script>
-    <script src="assets/js/bootstrap-carousel.js"></script>
-    <script src="assets/js/bootstrap-typeahead.js"></script>
-    
-    <!-- Load JS here for greater good =============================-->
-    <script src="assets/js/jquery-1.8.2.min.js"></script>
-    <script src="assets/js/jquery-ui-1.10.0.custom.min.js"></script>
-    <script src="assets/js/jquery.dropkick-1.0.0.js"></script>
-    <script src="assets/js/custom_checkbox_and_radio.js"></script>
-    <script src="assets/js/custom_radio.js"></script>
-    <script src="assets/js/jquery.tagsinput.js"></script>
-    <script src="assets/js/bootstrap-tooltip.js"></script>
-    <script src="assets/js/jquery.placeholder.js"></script>
-    <script src="http://vjs.zencdn.net/c/video.js"></script>
-    <script src="assets/js/application.js"></script>
-    <!--[if lt IE 8]>
+<!-- Load JS here for greater good =============================--> 
+<script src="assets/js/jquery-1.8.2.min.js"></script> 
+<script src="assets/js/jquery-ui-1.10.0.custom.min.js"></script> 
+<script src="assets/js/jquery.dropkick-1.0.0.js"></script> 
+<script src="assets/js/custom_checkbox_and_radio.js"></script> 
+<script src="assets/js/custom_radio.js"></script> 
+<script src="assets/js/jquery.tagsinput.js"></script> 
+<script src="assets/js/bootstrap-tooltip.js"></script> 
+<script src="assets/js/jquery.placeholder.js"></script> 
+<script src="http://vjs.zencdn.net/c/video.js"></script> 
+<script src="assets/js/application.js"></script> 
+<!--[if lt IE 8]>
       <script src="js/icon-font-ie7.js"></script>
       <script src="js/icon-font-ie7-24.js"></script>
     <![endif]-->
-  </body>
+</body>
 </html>
