ce7b4b85d20db052fa6255309fe71030c099fbc4
Dolan <dolan_miu@hotmail.com>
2013-04-07 05:47:39 +0100

    Added english to main page

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -13,18 +13,17 @@
 <div class="featurette"> <img class="featurette-image pull-right" src="assets/img/homepage-big1.jpg">
   <h2 class="featurette-heading">Raise money. <span class="muted">To support those in need.</span></h2>
   <a class="btn btn-large btn-success" href="#">I am a Charity</a>
-  <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+  <p class="lead">Is your charity looking for funding? Why not join our online donation system? It’s easy, safe and secure! Create your page and start raising money right now!</p>
 </div>
 <hr class="featurette-divider">
 <div class="featurette">
-  <h2 class="featurette-heading">Donate money. <span class="muted">See for yourself.</span></h2>
+        <h2 class="featurette-heading">Oh yeah, it's that good. <span class="muted">See for yourself.</span></h2>
   <a class="btn btn-large btn-success" href="#">I want to Donate</a>
-  <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
 </div>
 <hr>
 <div class="indexbanner">
 <!--<div class="row-fluid indexbanner"> -->
-  <img class="screenbanner" src="assets/img/homepage-jumbotron.png">
+	<div class="bannerpattern"></div>
   <div class="row">
     <div class="span3"> <img class="img-circle" data-src="holder.js/140x140">
       <h2>Who are we?</h2>
