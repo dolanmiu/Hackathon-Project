25492608ed0e03c29e1ea8a3970f6dbfc89fdcfc
SocialPledge <raphael.saunier@gmail.com>
2013-04-07 05:00:55 +0000

    Updated index page

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -13,18 +13,19 @@
 <div class="featurette"> <img class="featurette-image pull-right" src="assets/img/homepage-big1.jpg">
   <h2 class="featurette-heading">Raise money. <span class="muted">To support those in need.</span></h2>
   <a class="btn btn-large btn-success" href="#">I am a Charity</a>
-  <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+  <p class="lead">Is your charity looking for funding? Why not join our online donation system? It’s easy, safe and secure! Create your page and start raising money right now!</p>
 </div>
 <hr class="featurette-divider">
 <div class="featurette">
-  <h2 class="featurette-heading">Donate money. <span class="muted">See for yourself.</span></h2>
+  <h2 class="featurette-heading">Donate money. <span class="muted">Do good, feel good.</span></h2>
   <a class="btn btn-large btn-success" href="#">I want to Donate</a>
-  <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+  <p class="lead">By simply click on button above, find out the charity you are interested in and support them. Your personal information is secure, and your donation will be shared on Facebook to spread the love! We believe helping others is the ultimate form of positive karma. Do good for us all, whether it be saving a staring child, or contributing to the cure to cancer.</p>
 </div>
 <hr>
 <div class="indexbanner">
 <!--<div class="row-fluid indexbanner"> -->
-  <img class="screenbanner" src="assets/img/homepage-jumbotron.png">
+	<div class="bannerpattern"></div>
+  <!--<img class="screenbanner" src="assets/img/homepage-jumbotron.png">-->
   <div class="row">
     <div class="span3"> <img class="img-circle" data-src="holder.js/140x140">
       <h2>Who are we?</h2>
